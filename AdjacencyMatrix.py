ed880c89f637c3d8915ed4e2c2036ce719a96cd8
Alan Wood <awo1217@wgu.edu>
2024-05-16 07:09:45 -0400

    Altered truck and added code to main

--- AdjacencyMatrix.py
+++ AdjacencyMatrix.py
@@ -1,7 +1,56 @@
 from CSV import getrows
 
-distance_rows = getrows("WGUPS Distance Table (CSV).csv")
 
-needed_rows = distance_rows[7:]
+def getAdjacencyMatrix(file_path):
+    distance_rows = getrows(file_path)
 
-print(needed_rows)
+    # Gets the rows starting with row 8 and removes the first
+    # entry because it is not necessary
+    needed_rows = distance_rows[7:]
+    needed_rows[0][0] = ''
+
+    horizontal_address_list = []
+    vertical_address_list = []
+
+    # Cleans up the data in the first row and enters it into
+    # the horizontal address list
+    for item in needed_rows[0]:
+        if not item == '':
+            temp_list = item.splitlines()
+            address = temp_list[1].split(',')[0].lstrip()
+            horizontal_address_list.append(address)
+
+    # Adds a blank entry to the beginning of the horizontal address list
+    horizontal_address_list.insert(0, '')
+
+    # Cleans up the data in the first column of the distance rows
+    # and adds it to the vertical address list
+    for item in needed_rows:
+        if not item[1] == '':
+            temp_list = item[1].splitlines()
+            address = temp_list[0].lstrip()
+            vertical_address_list.append(address)
+
+    # Changes the first vertical entry from HUB to the address
+    # of the first entry in the horizontal address list
+    vertical_address_list[0] = horizontal_address_list[1]
+
+    # Retrieves just the rows where the distances are entered
+    distance_needed_rows = distance_rows[8:]
+
+    distance_matrix = []
+
+    # Places only the distances from each row into a distance matrix list
+    for row in distance_needed_rows:
+        distance_matrix.append(row[2:])
+
+    distance_matrix.insert(0, horizontal_address_list)
+
+    count = 0
+
+    for item in distance_matrix:
+        if not item[0] == '':
+            item.insert(0, vertical_address_list[count])
+            count = count + 1
+
+    return distance_matrix
--- Main.py
+++ Main.py
@@ -1,19 +1,24 @@
 from HashTable import HashTable
 from Truck import Truck
 from CSV import getrows
+from AdjacencyMatrix import getAdjacencyMatrix
 
+HUB = "4001 South 700 East"
 
 def runMainProgram():
     # Uses getrows function from CSV.py to retrieve the data
     # from the csv files.
     distance_rows = getrows("WGUPS Distance Table (CSV).csv")
     package_rows = getrows("WGUPS Package File (CSV).csv")
+    adjMatrix = getAdjacencyMatrix("WGUPS Distance Table (CSV).csv")
 
     # Isolates the package data and inserts it into
     # the hash table.
     package_dict = getPackageDict(package_rows)
     for key, data in package_dict.items():
         ht.insert(key, data)
+
+    loadTrucks(package_dict, adjMatrix)
 
     # Begin user interface
     print("********************************************")
@@ -47,9 +52,72 @@ def runMainProgram():
             boolean_in_program = False
 
 
-def travelAlgo(distance_rows):
+def loadTrucks(package_dict):
     truck1 = Truck(8, 0)
     truck2 = Truck(8, 0)
+
+    # Manually load trucks
+    truck1_list = [1, 2, 4, 5, 7, 8, 14, 15, 19, 20, 29, 30, 31, 34, 37, 40]
+    truck2_list = [3, 10, 12, 17, 18, 21, 22, 23, 24, 25, 26, 27, 33, 36, 38, 39]
+    truck3_list = [6, 9, 11, 13, 16, 28, 32, 35]
+
+    for package in truck1_list:
+        truck1.add_package(package, package_dict[package])
+
+    for package in truck2_list:
+        truck2.add_package(package, package_dict[package])
+
+    # early_package_dict = {}
+    # for key, data in package_dict.items():
+    #     if data[3] == '10:30 AM' or data[3] == '9:00 AM':
+    #         early_package_dict[key] = data[3]
+    #
+    # # Test to see how long it will take to deliver each of those packages
+    #
+    # nearest_to_furthest_dict = {}
+    #
+    # for key in early_package_dict.keys():
+    #     package_data = package_dict[key]
+    #     for address in adj_matrix:
+    #         if not address[0] == '':
+    #             if address[0] == package_data[0]:
+    #                 nearest_to_furthest_dict[key] = address[1]
+    #
+    # count = 0
+    # for data in nearest_to_furthest_dict.values():
+    #     count = count + float(data)
+    #
+    # print(count)
+
+
+def travelAlgo(truck):
+    adjMatrix = getAdjacencyMatrix('WGUPS Distance Table (CSV).csv')
+
+    distance_dict = {}
+
+    truck_package_dict = truck.package_dict
+
+    start_location = HUB
+    next_location = ''
+    distance_dict = {}
+
+    for key, value in truck_package_dict.items():
+        # Get address of package
+        next_location = value[0]
+        next_location_distance = 0.0
+        for row in adjMatrix:
+            if not row[0] == '':
+                if row[0] == next_location:
+                    for x in range(1, len(adjMatrix[0]) - 2):
+                        if adjMatrix[0][x] == start_location:
+                    #         Matching start location stopped here
+
+                    break
+                else:
+                    pass
+        distance_dict[key] = value
+
+
 
     #STOPPING HERE FOR THE EVENING
 
